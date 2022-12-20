04d4f8a6641df921740b8a04435a284c903f46b7
reza <rezasafaei70@gmail.com>
2022-12-20 21:51:58 +0330

    add listn

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
-
+// const io = new Server(server);
+const io = Server.listen(server)
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
